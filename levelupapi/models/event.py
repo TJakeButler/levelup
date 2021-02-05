7e91d56ab8dca65078be98f66aa80172d6ebaf0c
Jake Butler <tjakebutler@gmail.com>
2021-02-05 12:15:30 -0600

    first commit

---
+++ levelupapi/models/event.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Event(models.Model):
+
+   
+    event_time = models.DateField()
+    game = models.ForeignKey("Game", on_delete=models.CASCADE)
+    location = models.CharField(max_length=255)
+    scheduler = models.ForeignKey("Gamer", on_delete=models.CASCADE)
