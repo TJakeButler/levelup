7c19604b4b8968d78561d6e0bed9ae9e8b7210a0
Jake Butler <tjakebutler@gmail.com>
2021-02-09 15:40:51 -0600

    finished chapter 6 and 7

--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -9,4 +9,4 @@ class Game(models.Model):
     game_type = models.ForeignKey("GameType", on_delete=models.CASCADE)
     number_of_players = models.IntegerField()
     gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
-    description = models.CharField(max_length=255)
+    description = models.CharField(max_length=2000)
